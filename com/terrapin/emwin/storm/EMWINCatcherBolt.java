9d33e90426f929cae32aefae222d69a87bdbafa3
Paul Curtis <pcurtis@maprtech.com>
2013-08-18 15:21:28 -0400

    The MapR spout works A LOT better if you ack the tuple.

--- com/terrapin/emwin/storm/EMWINCatcherBolt.java
+++ com/terrapin/emwin/storm/EMWINCatcherBolt.java
@@ -3,13 +3,18 @@
  */
 package com.terrapin.emwin.storm;
 
+import java.util.Map;
+
 import org.apache.commons.lang.SerializationUtils;
 
 import com.terrapin.emwin.object.Packet;
 
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
@@ -18,7 +23,7 @@ import backtype.storm.tuple.Values;
  * @author pcurtis
  *
  */
-public class EMWINCatcherBolt extends BaseBasicBolt {
+public class EMWINCatcherBolt extends BaseRichBolt {
 
     /**
      * 
@@ -27,13 +32,16 @@ public class EMWINCatcherBolt extends BaseBasicBolt {
         // TODO Auto-generated constructor stub
     }
 
+    private OutputCollector collector;
+    
     /* (non-Javadoc)
      * @see backtype.storm.topology.IBasicBolt#execute(backtype.storm.tuple.Tuple, backtype.storm.topology.BasicOutputCollector)
      */
     @Override
-    public void execute(Tuple input, BasicOutputCollector collector) {
+    public void execute(Tuple input) {
         Packet p = (Packet)SerializationUtils.deserialize(input.getBinaryByField("msg"));
         collector.emit(new Values(p, p.ft));
+        collector.ack(input);
     }
 
     /* (non-Javadoc)
@@ -42,6 +50,14 @@ public class EMWINCatcherBolt extends BaseBasicBolt {
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
         declarer.declare(new Fields("packet", "type"));
+    }
+
+
+    @Override
+    public void prepare(Map stormConf, TopologyContext context,
+            OutputCollector collector) {
+        this.collector = collector;
+        
     }
 
 }
